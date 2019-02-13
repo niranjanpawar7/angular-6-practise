f14aac2c6e2b857102f7e870a16a6c726dfaa945
Niranjan Pawar <pkumar@prohaktiv.com>
2019-02-13 15:32:53 +0530

    update user in node side

--- src/app/my-dialog/my-dialog.component.ts
+++ src/app/my-dialog/my-dialog.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Inject } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material';
+import { UpdateUserService } from '../shared/services/update-user.service';
+import {Router} from "@angular/router"
+
 
 @Component({
   selector: 'app-my-dialog',
@@ -9,13 +13,60 @@ import { MAT_DIALOG_DATA } from '@angular/material';
 })
 export class MyDialogComponent implements OnInit {
   
+  myForm : FormGroup;
   modalTitle;
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
+  formData;
+  submitted = false;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private fb: FormBuilder, private update : UpdateUserService, private route: Router) {
         this.modalTitle = data.title;
-        console.log(data)
+        this.formData = data.data;  
     }
 
-  ngOnInit() {
+  ngOnInit() { 
+     
+    this.myForm = this.fb.group({
+      id : [this.formData.id, Validators.required],
+      username : [this.formData.username, Validators.required],
+      contact : [this.formData.contact, Validators.required],
+      email: [this.formData.email, [Validators.required, Validators.email]],
+      password: [this.formData.password, [Validators.required, Validators.minLength(3)]],
+      confpassword: [this.formData.confpassword, [Validators.required, Validators.minLength(3)]]
+    }); 
+    let UserData = this.data.data
+    console.log('this.formData', UserData)
+  };
+
+  get f() { return this.myForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.myForm.invalid) {
+            console.log('Invalid');
+            return;
+          } else {
+            debugger
+          const formData = this.myForm.value ;
+          if (formData.password === formData.confpassword) { 
+            this.submitted = true;
+            this.update.updateUserData(formData)
+            .subscribe(
+              (response) => {
+                console.log('response', response);
+               alert('success updates')
+              },
+              (error) => {
+                console.log('error', error);
+              },
+            )
+          
+          } else {
+            console.log('Wrong password');
+            this.submitted = false;
+          }
+        }
   }
 
 }
